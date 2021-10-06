7dd360800b94cde8a3dd17f079f49d0e4d4be35c
Cristian Preciado Sanchez <cristianpreciadosanchez@MacBook-Pro-de-Cristian.local>
2021-10-05 23:08:21 -0500

    creacion de api, consumo post

---
+++ apiCalculadora/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using apiCalculadora.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+namespace apiCalculadora.Context
+{
+    public class AppDbContext : DbContext
+    {
+       public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<LogCalculo> log_calculos { get; set; }
+    }
+}
---
+++ apiCalculadora/Models/LogCalculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace apiCalculadora.Models
+{
+    public class LogCalculo
+    {
+        [Key]
+        public int id { get; set; }
+
+        public int valorUno { get; set; }
+
+        public int valorDos { get; set; }
+
+        public int resultado { get; set; }
+    }
+}
